bbfa4bd57deddab68106ea21839e83668b3ff752
Khutso <khutso95@gmail.com>
2023-08-30 21:52:06 +0200

    Added missing files
    Added Controllers and Models folder

---
+++ Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project2_27593657.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public short OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public short CustomerId { get; set; }
+        public string? DeliveryAddress { get; set; }
+
+        public virtual Customer Customer { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project2_27593657.Models
+{
+    public partial class OrderDetail
+    {
+        public short OrderDetailsId { get; set; }
+        public short OrderId { get; set; }
+        public short ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int? Discount { get; set; }
+
+        public virtual Order Order { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
